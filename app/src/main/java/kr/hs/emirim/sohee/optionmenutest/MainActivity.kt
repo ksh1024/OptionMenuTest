49807afe7b1b0819a045c2c2c4b2f323e536bb6d
ksh25 <ksh251024@naver.com>
2023-08-24 19:17:43 +0900

    optionMenu 실습7-1 완성

--- app/src/main/java/kr/hs/emirim/sohee/optionmenutest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/sohee/optionmenutest/MainActivity.kt
@@ -1,9 +1,11 @@
 package kr.hs.emirim.sohee.optionmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.LinearLayout
 
@@ -24,5 +26,36 @@ class MainActivity : AppCompatActivity() {
         var mInflater = menuInflater
         mInflater.inflate(R.menu.menu1,menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.itemRed->{
+                linear.setBackgroundColor(Color.RED)
+                return true
+            }
+            R.id.itemBlue->{
+                linear.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            R.id.itemMagenta->{
+                linear.setBackgroundColor(Color.MAGENTA)
+                return true
+            }
+            R.id.subRotate->{
+               btn.rotation += 45f //시계 방향 45도 회전
+                return true
+            }
+            R.id.subSize->{
+                btn.scaleX *= 2f //너비 두배
+                return true
+            }
+            R.id.subInit->{
+                btn.rotation = 0f
+                btn.scaleX = 1f
+                return true
+            }
+        }
+        return false
     }
 }
